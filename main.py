4009a786aa5a857396eae2bd1fce599addc08464
Duleepa J Thrimawithana <d.thrimawithana@auckland.ac.nz>
2020-08-29 22:36:48 +0000

    test

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import string
 
 format_string = "Hello {:^25}, how are you? Is your last name {}?"
-print(format_string.format("Duleepa","Thrimawithana"))
+print(format_string.format("Duleepa", "Thrimawithana"))
 print("")
 
-template = string.Template("Hello $first, how are you? Is your last name $last?")
-print(template.substitute(first="Duleepa",last="Thrimawithana"))
+template = string.Template(
+    "Hello $first, how are you? Is your last name $last?")
+print(template.substitute(first="Duleepa", last="Thrimawithana"))
 
 number = int(input("Enter Number: "))
-print(number*2)
+print(number == 50)
+
+for i in range(0, 10):
+    print(i, ' ', end='')
+    if i == 5:
+        break
+print()
