e0917cdd0bcf167056fc532c91514dbe199f548f
Duleepa J Thrimawithana <d.thrimawithana@auckland.ac.nz>
2020-08-29 21:37:46 +0000

    Added int read

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import string
+
 format_string = "Hello {:^25}, how are you? Is your last name {}?"
 print(format_string.format("Duleepa","Thrimawithana"))
+print("")
+
+template = string.Template("Hello $first, how are you? Is your last name $last?")
+print(template.substitute(first="Duleepa",last="Thrimawithana"))
+
+number = int(input("Enter Number: "))
+print(number*2)
